1c0f4e6876e0474ba13b928fc226a9434950680a
Vasyl Vanchuk <vasyl.vanchuk01@dixonscarphone.com>
2021-06-14 20:01:52 +0300

    First commit for new files and dirs

---
+++ modules/network/main.tf
@@ -0,0 +1,40 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_virtual_network" "aksvn" {
+  name                = "aksvn"
+  address_space       = ["10.52.0.0/16"]
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "aksnvn_sna" {
+  name                 = "${azurerm_virtual_network.aksvn.name}-sna"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.aksvn.name
+  address_prefixes     = ["10.52.0.0/24"]
+}
+
+resource "azurerm_subnet" "aksnvn_snb" {
+  name                 = "${azurerm_virtual_network.aksvn.name}-snb"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.aksvn.name
+  address_prefixes     = ["10.52.1.0/24"]
+}
+
+resource "azurerm_subnet" "aksnvn_snc" {
+  name                 = "${azurerm_virtual_network.aksvn.name}-snc"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.aksvn.name
+  address_prefixes     = ["10.52.2.0/24"]
+}
+
+
+output "aksvn_id" {
+  value = azurerm_virtual_network.aksvn.id
+}
+
+output "aksnc_sn_id" {
+  value = azurerm_subnet.aksnvn_snb.id
+}
---
+++ modules/network/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+#variable "client_id" {}
+
+#variable "client_secret" {}
